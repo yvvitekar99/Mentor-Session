376ac7cc3aa8654878538357cb49a955ec5040d1
itz-deep02 <57581001+itz-deep02@users.noreply.github.com>
2022-05-29 14:26:08 +0530

    Create queue.java

---
+++ queue.java
@@ -0,0 +1,51 @@
+class Queue {
+       static int arr[];
+       static int size;
+       static int rear;
+ 
+       Queue(int size) {
+           this.size = size;
+           arr = new int[size];
+           rear = -1;
+       }
+ 
+       public static boolean isEmpty() {
+           return rear == -1;
+       }
+ 
+       public static boolean isFull() {
+           return rear == size-1;
+       }
+ 
+       public static void enqueue(int data) {
+           if(isFull()) {
+               System.out.println("Overflow");
+               return;
+           }
+ 
+           arr[++rear] = data;
+       }
+ 
+    
+       public static int dequeue() {
+           if(isEmpty()) {
+               System.out.println("empty queue");
+               return -1;
+           }
+           int front = arr[0];
+           for(int i=0; i<rear; i++) {
+               arr[i] = arr[i+1];
+           }
+           return front;
+       }
+ 
+       public static int front() {
+           if(isEmpty()) {
+               System.out.println("empty queue");
+               return -1;
+           }
+          
+           return arr[0];
+       }
+
+   }
